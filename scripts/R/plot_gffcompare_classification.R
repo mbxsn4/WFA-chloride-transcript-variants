d86d0bf7f8147c77dd0b0fa9225dd148375a81b1
Sahar Naeemi <mbxsn4@nottingham.ac.uk>
2026-07-14 21:31:09 +0100

    Add reproducible GffCompare classification plotting script

---
+++ scripts/R/plot_gffcompare_classification.R
@@ -0,0 +1,43 @@
+classification <- c(
+  "Class j\nnovel isoforms",
+  "Exact matches\n(=)",
+  "Intergenic\n(u)",
+  "Contained\n(c)",
+  "Reference containment\n(k)",
+  "Intronic\n(i)",
+  "Opposite strand\n(x)"
+)
+
+count <- c(20686, 17071, 13038, 4269, 3480, 2616, 1623)
+
+png("gffcompare_transcript_classification.png",
+    width = 2400, height = 1500, res = 300, type = "cairo")
+
+par(mar = c(5, 10, 4, 2))
+
+barplot(
+  rev(count),
+  names.arg = rev(classification),
+  horiz = TRUE,
+  las = 1,
+  xlab = "Transcript count",
+  main = "GffCompare transcript classification distribution"
+)
+
+dev.off()
+
+pdf("gffcompare_transcript_classification.pdf",
+    width = 8, height = 5)
+
+par(mar = c(5, 10, 4, 2))
+
+barplot(
+  rev(count),
+  names.arg = rev(classification),
+  horiz = TRUE,
+  las = 1,
+  xlab = "Transcript count",
+  main = "GffCompare transcript classification distribution"
+)
+
+dev.off()
